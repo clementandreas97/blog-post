b8c4006905150c0d543d2121950d20f2de800c5c
Christopher Clement Andreas <clement.andreas97@gmail.com>
2019-08-31 15:52:40 +0700

    Initial commit with draft Models

---
+++ models/comment.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const commentSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    created_at: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    parent: {
+        type: String
+    }
+})
---
+++ models/post.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const Comment = require('./comment')
+const postSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    created_at: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    comments: {
+        type: [Comment],
+        required: true,
+        default: []
+    }
+})
